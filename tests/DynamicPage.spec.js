9c11ae0fff62891d489c8f393867e96d0f49c321
Viren Chauhan <vir3.chauhan@gmail.com>
2025-01-28 12:39:54 +0530

    Wait Dynamic
    Create new class for dynamic wait locator

--- tests/DynamicPage.spec.js
+++ tests/DynamicPage.spec.js
@@ -1,8 +1,11 @@
 const { test, expect } = require('@playwright/test');
 
-test('Browser context Playwright', async ({ browser }) => {
-    const context = await browser.newContext();
-    const page = await context.newPage();
+test('Browser context Playwright', async ({ page }) => {
+    
+    await page.goto('https://rahulshettyacademy.com/client');
+
+    // const context = await browser.newContext();
+    // const page = await context.newPage();
     
     //Locators
     const userName = page.locator("#userEmail");
@@ -11,18 +14,21 @@ test('Browser context Playwright', async ({ browser }) => {
     const cardTitles = page.locator(".card-body h5 b");
 
     //URL
-    await page.goto('https://rahulshettyacademy.com/client');
+    
     console.log(await page.title());
 
     // css
     await userName.fill("qaops@yopmail.com");
     await password.fill("Test@12345");    
     await signIn.click();
-    
+    //This is waitload
     await page.waitForLoadState('networkidle');
+    /* if networkidle wait is not working we can use waitfor */
+    //await page.locator(".card-body h5 b").last().waitFor();
+    
     const allTitles = await cardTitles.allTextContents();
     console.log(allTitles);
     
     console.log(await cardTitles.first().textContent());    
-    console.log(await cardTitles.nth(0).textContent()) 
+    console.log(await cardTitles.nth(4).textContent()) 
 });
