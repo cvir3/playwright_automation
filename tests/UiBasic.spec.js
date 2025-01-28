1086f2d9ac2957aa3f1f1782a5993aa7d069cbd2
Viren Chauhan <vir3.chauhan@gmail.com>
2025-01-28 15:14:20 +0530

    Handling Dropdown, Radiobtn

--- tests/UiBasic.spec.js
+++ tests/UiBasic.spec.js
@@ -26,3 +26,31 @@ test('Browser context Playwright', async ({ browser }) => {
     console.log(await cardTitles.first().textContent());    
     console.log(await cardTitles.nth(0).textContent()) 
 });
+
+test.only('UI Controls_DropDown_RadioBTN', async ({page}) =>
+{
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    const userName = page.locator('#username');
+    const password = page.locator("[type='password']");
+    const dropdown = page.locator("select.form-control");
+
+    await dropdown.selectOption("Teacher");    
+
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click()
+    console.log(await page.locator(".radiotextsty").last().isChecked());
+    //This is assertions
+    expect(page.locator(".radiotextsty").last()).toBeChecked();
+
+    await page.locator(".radiotextsty").first().click();    
+
+    await page.locator("#terms").click();
+    await expect(page.locator("#terms")).toBeChecked();
+    // await page.pause();
+    await page.locator("#terms").uncheck();
+    expect(await page.locator("#terms").isChecked()).toBeFalsy();
+
+
+    
+
+});
