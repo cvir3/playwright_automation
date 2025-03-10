4fc66d981e05d61d51f482445b235456e1d95a10
cvir3 <vir3.chauhan@gmail.com>
2025-03-10 12:35:07 +0530

    Refactor API Calls from utils folder and isolate from Web test logic

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -1,43 +1,22 @@
 const { test, expect, request } = require('@playwright/test');
+const { API_Utilis } = require('./utilis/API_Utilis'); // This import is necessary to use the API_Utilis class
+
 const loginPayLoad = { userEmail: "qaops@yopmail.com", userPassword: "Test@12345" };
 const orderPayLoad = { "orders": [{ "country": "Cuba", "productOrderedId": "67a8dde5c0d3e6622a297cc8" }] }
-let orderId = "67a8dde5c0d3e6622a297cc8";
-let token;
+
+let response;
 
 test.beforeAll(async () => {
     const apiContext = await request.newContext();
-    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
-        {
-            data: loginPayLoad
-        }
-    )
-    expect(loginResponse.ok()).toBeTruthy();
-
-    const loginResponseJson = await loginResponse.json();
-    token = loginResponseJson.token;
-    console.log(token);
-
-    //
-    const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
-        {
-            data: orderPayLoad,
-            headers: {
-                'Authorization': token,
-                'Content-Type': 'application/json'
-            },
-
-        })
-    const orderResponseJson = await orderResponse.json();
-    console.log(orderResponseJson);
-    orderId = orderResponseJson.orders[0]
-
+    const api_Utilis = new API_Utilis(apiContext, loginPayLoad);
+    response = await api_Utilis.createOrder(orderPayLoad);
 });
 
 //Create order is success
 test('Client App Login', async ({ page }) => {
     page.addInitScript(value => {
         window.localStorage.setItem("token", value);
-    }, token);
+    }, response.token);
 
     await page.goto('https://rahulshettyacademy.com/client');
     const btnOrder = page.locator("button[routerlink*='myorder']");
@@ -48,13 +27,13 @@ test('Client App Login', async ({ page }) => {
 
     for (let i = 0; i < await rows.count(); ++i) {
         const rowOrderId = await rows.nth(i).locator("th").textContent();
-        if (orderId.includes(rowOrderId)) {
+        if (response.orderId.includes(rowOrderId)) {
             await rows.nth(i).locator("button").first().click();
             break;
         }
     }
     const orderIdDetails = await page.locator(".col-text").textContent();
     await page.pause();
-    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+    expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
 
 });
