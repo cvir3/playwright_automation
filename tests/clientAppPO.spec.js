40810a135a148c17eee6aa2a64aa3807846917b9
cvir3 <vir3.chauhan@gmail.com>
2025-03-17 20:05:21 +0530

    Drive the data from external json files

--- tests/clientAppPO.spec.js
+++ tests/clientAppPO.spec.js
@@ -1,19 +1,19 @@
 const { test, expect } = require('@playwright/test');
 const { POManager } = require('../pageObject/POManager');
+//Json -> Sting -> JS Object
+const dataset = JSON.parse(JSON.stringify(require("../utilis/TestData.json")));
 
 
 test('@Client App login', async ({ page }) => {
 
     const poManager = new POManager(page);
-    const username = "qaops@yopmail.com";
-    const password = "Test@12345"
-    const productName = 'ZARA COAT 3';
+
     const loginpage = poManager.getLoginPage();
     await loginpage.goTo();
-    await loginpage.validLogin(username, password);
+    await loginpage.validLogin(dataset.username, dataset.password);
     const dashboardPage = poManager.getDashboardPage();
     await page.waitForTimeout(2000);
-    await dashboardPage.searchProductAddCart(productName);
+    await dashboardPage.searchProductAddCart(dataset.productName);
     await dashboardPage.navigateToCart();
     await page.waitForLoadState('networkidle');
     const bool = await page.locator("h3:has-text('zara coat 3')").isVisible();
@@ -21,7 +21,7 @@ test('@Client App login', async ({ page }) => {
     await page.locator("text=Checkout").click();
     await page.locator("[placeholder*='Country']").fill("ind");
     const dropdown = page.locator(".ta-results");
-    await dropdown.waitFor();
+    await dropdown.waitFor(2000);
     const optionsCount = await dropdown.locator("button").count();
     for (let i = 0; i < optionsCount; ++i) {
         const text = await dropdown.locator("button").nth(i).textContent();
@@ -30,7 +30,7 @@ test('@Client App login', async ({ page }) => {
             break;
         }
     }
-    await expect(page.locator(".user__name [type='text']").first()).toHaveText(username);
+    await expect(page.locator(".user__name [type='text']").first()).toHaveText(dataset.username);
     await page.locator(".action__submit").click();
     await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
     const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
