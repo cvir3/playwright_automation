820414a85768d8a699744a8776aa829f4e9a6eeb
cvir3 <vir3.chauhan@gmail.com>
2025-03-17 14:51:56 +0530

    Added WaitForTimeout

--- tests/clientApp.spec.js
+++ tests/clientApp.spec.js
@@ -14,15 +14,14 @@ test('@Client App login', async ({ page }) => {
     await loginpage.goTo();
     await loginpage.validLogin(username, password);
     const dashboardPage = new DashboardPage(page);
+    await page.waitForTimeout(2000);
     await dashboardPage.searchProductAddCart(productName);
-
-    // await page.pause();
     await dashboardPage.navigateToCart();
     await page.waitForLoadState('networkidle');
     const bool = await page.locator("h3:has-text('zara coat 3')").isVisible();
     expect(bool).toBeTruthy();
     await page.locator("text=Checkout").click();
-    await page.locator("[placeholder*='Country']").fill("ind");
+    await page.locator("[placeholder*='Country']").fill("India");
     const dropdown = page.locator(".ta-results");
     await dropdown.waitFor();
     const optionsCount = await dropdown.locator("button").count();
@@ -33,7 +32,7 @@ test('@Client App login', async ({ page }) => {
             break;
         }
     }
-    await expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+    await expect(page.locator(".user__name [type='text']").first()).toHaveText(username);
     await page.locator(".action__submit").click();
     await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
     const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
