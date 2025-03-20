71cc6ebfdb61a7800f2ad3ae9570d1cd6b1d39df
cvir3 <vir3.chauhan@gmail.com>
2025-03-20 12:33:12 +0530

    Added Fnction

--- tests/TsDemo.ts
+++ tests/TsDemo.ts
@@ -10,3 +10,12 @@ let numbers: number[] = [1, 2, 3];
 
 let data: any = "This is a string";
 data = 10;
+
+
+// Functions
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+add(3, 4);
+console.log(add(3, 4));
