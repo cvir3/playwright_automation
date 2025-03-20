92febdd2b5e953cf41b2e6ac6e7efd66cd529692
cvir3 <vir3.chauhan@gmail.com>
2025-03-20 12:07:54 +0530

    TypeScript Basic Syntaxes V1
    Installed Typescript and updated package.json

---
+++ tests/TsDemo.ts
@@ -0,0 +1,12 @@
+let message = "Hello, World!";
+console.log(message);
+
+let age: number = 30;
+console.log(age);
+
+let isActive: boolean = false;
+
+let numbers: number[] = [1, 2, 3];
+
+let data: any = "This is a string";
+data = 10;
